ec61202f5bb44369c01d94530007d35d5f92bc9c
fletcherkeeley <fletcher@tiehackworks.com>
2025-08-04 10:05:22 -0600

    Adding exercises for Data Types chapter

---
+++ data_types/data_types_exercises.py
@@ -0,0 +1,34 @@
+# Instructions: Identify the data type or class for each of the following values:
+
+'True'      # String,  'str'
+False       # Boolean, 'bool'
+(1, 2, 3)   # Tuple,   'tuple'
+1.5         # Float,   'float
+[1, 2, 3]   # List,    'list'
+2           # Integer, 'int'
+range(5)    # Range,   'range'
+{1, 2, 3}   # Set,     'set'
+None        # 'NoneType', 'NoneType'
+{'foo': 'bar', } # Dictionary, 'dict'
+
+
+# Instructions: Create a tuple called 'names' that contains several pet names. 
+
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+
+
+# Instructions: Create a dictionary named 'pets' that contains a list of pet names and the tyep of animal. 
+
+pets = {
+    'Asta':         'dog',
+    'Butterscotch': 'cat',
+    'Pudding':      'cat',
+    'Neptune':      'fish',
+    'Darwin':       'lizard',
+}
